ac38ba7e18baf18b40a6f8efa674aa017ac7de40
shermscissors <christian.gales09@gmail.com>
2022-02-14 13:39:52 -0500

    Initialize

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//Import React Router Dom
+//Import axios or backend interacting part here
+
+function Home(props) {
+    return (
+        <div>
+            <div className='Home_Container'>
+                <h1 className='Home_Logo_Container'> Pee <span className='Home_Logo_P_Span'>P</span> </h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
