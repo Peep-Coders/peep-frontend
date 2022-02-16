4887cc1dbad3fa37080006e6e83ea19368b80c05
Andrew Kal <79432338+AndrewKalGit@users.noreply.github.com>
2022-02-16 17:07:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,56 +9,14 @@ import Login from './components/Login/Login';
 import Logout from './components/Logout/Logout';
 import Navigation from './components/Navigation/Navigation';
 import People from './components/People/People';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import PostDetails from './components/PostDetails/PostDetails'
-import Posts from './components/Posts/Posts';
 import Register from './components/Register/Register';
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 8503c5d (Posts view all sample posts)
-import Register from './components/Register/Register';
-import Post from './components/Post/Post';
+// import Post from './components/Post/Post';
 import PostDetails from './components/PostDetails/PostDetails';
-<<<<<<< HEAD
-=======
-import PostDetails from './components/PostDetails/PostDetails'
-import Posts from './components/Posts/Posts';
->>>>>>> 9e316dd (frontend for making post with images)
->>>>>>> 51fe654 (frontend for making post with images)
-=======
->>>>>>> 8503c5d (Posts view all sample posts)
-=======
-=======
->>>>>>> a4f4bf3 (front end submit image and description post)
-import Register from './components/Register/Register';
-import Post from './components/Post/Post';
-import PostDetails from './components/PostDetails/PostDetails';
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 96aa50f (front end submit image and description post)
-import PostDetails from './components/PostDetails/PostDetails'
-import Posts from './components/Posts/Posts';
-import Register from './components/Register/Register';
-<<<<<<< HEAD
-import Posts from './components/Posts/Posts';
-import PostDetails from './components/PostDetails/PostDetails';
->>>>>>> 6f6b76e (front end image and description submit)
->>>>>>> c373893 (front end image and description submit)
-<<<<<<< HEAD
->>>>>>> 3dff65f (front end image and description submit)
-=======
-=======
->>>>>>> 96aa50f (front end submit image and description post)
->>>>>>> a4f4bf3 (front end submit image and description post)
 
 function App() {
   return (
 		<>
+			<Navigation />
 			<Routes>
 				<Route index path='/' element={<Home />} />
 				<Route path='/login' element={<Login />} />
@@ -68,23 +26,8 @@ function App() {
 				<Route path='/header' element={<Header />} />
 				<Route path='/logout' element={<Logout />} />
 				<Route path='/people' element={<People />} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-				<Route path='/postdetails' element={<PostDetails/>} />
-				<Route path='/posts' element={<Posts />} />
-=======
-<<<<<<< HEAD
 				{/* <Route path='/post' element={<Post />} /> */}
 				<Route path='/postDetails' element={<PostDetails />} />
-=======
-				<Route path='/postdetails' element={<PostDetails/>} />
-				<Route path='/posts' element={<Posts />} />
->>>>>>> 9e316dd (frontend for making post with images)
->>>>>>> 51fe654 (frontend for making post with images)
-=======
-				<Route path='/postdetails' element={<PostDetails/>} />
-				<Route path='/posts' element={<Posts />} />
->>>>>>> 8503c5d (Posts view all sample posts)
 			</Routes>
 			<Footer />
 		</>
