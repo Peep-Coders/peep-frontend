5d99e0766b2bd9cde514c03e2e27de3a10cf02ea
shermscissors <christian.gales09@gmail.com>
2022-02-17 15:04:33 -0500

    Fixed Folders

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,17 +3,23 @@ import styles from './Footer.module.css';
 
 function Footer(props) {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 009a50f (Fixed Folders)
 	return (
 		<div className={styles.footerContainer}>
 			<span> PEEP &copy; | Created by PeepTeam </span>
 		</div>
 	);
+<<<<<<< HEAD
 =======
     return (
     <>
     </>
     );
 >>>>>>> 00a6040 (More CSS)
+=======
+>>>>>>> 009a50f (Fixed Folders)
 }
 
 export default Footer;
