14eafec34392611e7053589140c5782394e918d3
Andrew Kal <andrewkalgit@gmail.com>
2022-07-28 17:03:07 -0400

    tailwind boilerplate

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -32,6 +32,40 @@ function Navigation({setLoggedIn, loggedIn}, props) {
 	};
 	
 	return (
+	<header class="shadow-sm">
+	<div
+    class="flex items-center justify-between h-16 max-w-screen-xl px-4 mx-auto"
+  	>
+   
+
+      <form class="hidden mb-0 lg:flex ml-5">
+        <div class="relative">
+          <input
+            class="h-10 pr-10 text-sm placeholder-gray-300 border-gray-200 rounded-lg focus:z-10"
+            placeholder="Search..."
+            type="text"
+          />
+
+          <button
+            class="absolute inset-y-0 right-0 p-2 mr-px text-gray-600 rounded-r-lg"
+            type="submit"
+          >
+            <svg
+              class="w-5 h-5"
+              fill="currentColor"
+              viewbox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                clip-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                fill-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </form>
+	  </div>
 		<div className={styles.nav_bar}>
 			<div className={styles.btnContainer}>
 				<button className={styles.nav_button}>
@@ -64,6 +98,7 @@ function Navigation({setLoggedIn, loggedIn}, props) {
 				)}
 			</div>
 		</div>
+	</header>
 	);
 }
 
